96945f3d2c939f17c17aca43458e5435b236b00f
Rafael <rafsalas1987@hotmail.com>
2021-01-24 18:34:25 -0600

    temp

--- reverse/reverse.c
+++ reverse/reverse.c
@@ -4,8 +4,51 @@
 #include <sys/time.h>
 #include <assert.h>
 #include <string.h>
+#define VECTOR_SIZE 5
 
+//make sure to include error catching for malloc
 
+struct node{
+	const char* data;
+	struct node* next;
+};
+
+struct node *headNode = NULL;
+
+int nodeCount=0;
+
+struct node* addNode(const char* data){
+	printf("newData %s", data);
+	struct node * newNode = (struct node*)malloc(sizeof (struct node));
+	newNode->data= data;
+	newNode->next =headNode;
+	headNode = newNode;
+	nodeCount++;
+	return newNode;
+}
+
+void printList(){
+	struct node *currentNode = headNode;
+	
+	while(currentNode!=NULL){
+		printf("list data: %s",currentNode->data);
+		currentNode = currentNode->next;
+		
+	}
+	
+}
+
+int vectorSize= VECTOR_SIZE;
+char **stringVector;
+int currentIndex =0;
+
+int resizedVector(){
+	char** temp = (char**)malloc(sizeof(char*)*2*vectorSize);
+	memcpy(temp, stringVector, vectorSize);
+	temp[0] =NULL;
+	vectorSize=2*vectorSize;
+	return 0;
+}
 
 void printScreen(FILE *inputf, FILE *outputf){
 		char *line=NULL;
@@ -50,19 +93,43 @@ void printScreen(FILE *inputf, FILE *outputf){
 
 int main(int argc, char* argv[]){
 
-
-	// char** inLines;
-	// char** outLines;
+	
+	stringVector = (char**)malloc(sizeof(char*)*vectorSize);
 	if(argc>3){
 		fprintf(stderr, "usage: reverse <input> <output>\n");
 		exit(1);
 	}
-	else if(argc<1){
+	else if(argc<2){
 		printf("Please input your words:\n");
-		//input code
-		//input = ;
-		//
-		printf("\n");
+
+		char * buffer;
+		size_t length=0;
+		int size =0;
+		while(size!=-1){
+			size =getline(&buffer,&length,stdin);
+			if(size ==-1)break;
+			if(currentIndex ==vectorSize){
+				resizedVector();
+			}
+			char* tmpBuffer= (char*)malloc(sizeof(char)*size);
+			memcpy(tmpBuffer,buffer,size);
+			stringVector[currentIndex]=tmpBuffer;
+			printf("%s",stringVector[currentIndex]);
+			currentIndex++;
+
+			printf("%s",buffer); 
+		}
+		//currentIndex--;
+		printf("\n%d\n",currentIndex);
+		printf("\n---------------------");
+		
+		for(int i =0;i<currentIndex;++i){
+			char* temp= stringVector[i];
+			printf("%d %s",i,temp);
+		}
+		//printList();
+		//printf("\n---------------------");
+		
 	}
 	else
 	{
@@ -100,6 +167,6 @@ int main(int argc, char* argv[]){
 		
 	}
 		
-
+	printf("end of file\n");
 	return 0;
 }
