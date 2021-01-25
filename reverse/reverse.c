86a87fd02a0760e3bd5e62242161e7f27b2256e5
Rafael <rafsalas1987@hotmail.com>
2021-01-24 20:59:32 -0600

    ready for testing

--- reverse/reverse.c
+++ reverse/reverse.c
@@ -4,39 +4,8 @@
 #include <sys/time.h>
 #include <assert.h>
 #include <string.h>
-#define VECTOR_SIZE 5
+#define VECTOR_SIZE 10
 
-//make sure to include error catching for malloc
-
-struct node{
-	const char* data;
-	struct node* next;
-};
-
-struct node *headNode = NULL;
-
-int nodeCount=0;
-
-struct node* addNode(const char* data){
-	printf("newData %s", data);
-	struct node * newNode = (struct node*)malloc(sizeof (struct node));
-	newNode->data= data;
-	newNode->next =headNode;
-	headNode = newNode;
-	nodeCount++;
-	return newNode;
-}
-
-void printList(){
-	struct node *currentNode = headNode;
-	
-	while(currentNode!=NULL){
-		printf("list data: %s",currentNode->data);
-		currentNode = currentNode->next;
-		
-	}
-	
-}
 
 int vectorSize= VECTOR_SIZE;
 char **stringVector;
@@ -44,13 +13,66 @@ int currentIndex =0;
 
 int resizedVector(){
 	char** temp = (char**)malloc(sizeof(char*)*2*vectorSize);
-	memcpy(temp, stringVector, vectorSize);
-	temp[0] =NULL;
+	if (temp ==NULL){
+		fprintf(stderr, "Failed to resize stringVector array.\n");
+		exit(1);
+	}
+	memcpy(temp, stringVector, vectorSize*sizeof(char**));
 	vectorSize=2*vectorSize;
+	stringVector =temp;
+	temp=NULL;
 	return 0;
 }
 
-void printScreen(FILE *inputf, FILE *outputf){
+void reverseInput(FILE *inputf, FILE *outputf){
+		
+
+		char * buffer;
+		size_t length=0;
+		int size =0;
+		int lastLength=0;
+		while(size!=-1){
+			lastLength =size;
+			if(inputf != NULL){
+				size =getline(&buffer,&length,inputf);			
+			}
+			else
+			{
+				size =getline(&buffer,&length,stdin);
+			}
+
+			if(size ==-1)break;
+			if(currentIndex ==vectorSize-1){
+				resizedVector();
+			}
+			char* tmpBuffer= (char*)malloc(sizeof(char)*size);
+			if (tmpBuffer ==NULL){
+				fprintf(stderr, "Failed to allocate line memory.\n");
+				exit(1);
+			}
+			memcpy(tmpBuffer,buffer,size);
+			stringVector[currentIndex]=tmpBuffer;
+
+			currentIndex++;
+		}
+		
+		for(int i =currentIndex-1;i>=0;--i){
+			char* temp= stringVector[i];
+			if(outputf == NULL){			
+				if (i == currentIndex-1 && stringVector[i][lastLength-1]=='\n'){printf("\n");}
+				printf("%s",temp);
+				if (i == currentIndex-1 && stringVector[i][lastLength-1]!='\n'){printf("\n");}
+			}
+			else{
+				if (i == currentIndex-1 && stringVector[i][lastLength-1]=='\n'){fputs("\n",outputf);}
+				fputs(temp,outputf);
+				if (i == currentIndex-1 && stringVector[i][lastLength-1]!='\n'){fputs("\n",outputf);}
+			}
+		}
+	
+}
+
+void printFileScreen(FILE *inputf, FILE *outputf){
 		char *line=NULL;
 		size_t length=0;
 		int lineCount =0;
@@ -91,82 +113,65 @@ void printScreen(FILE *inputf, FILE *outputf){
 }
 
 
+
+
 int main(int argc, char* argv[]){
 
 	
 	stringVector = (char**)malloc(sizeof(char*)*vectorSize);
+	if (stringVector ==NULL){
+		fprintf(stderr, "Failed to allocate stringVector.\n");
+		exit(1);
+	}
 	if(argc>3){
 		fprintf(stderr, "usage: reverse <input> <output>\n");
 		exit(1);
 	}
 	else if(argc<2){
 		printf("Please input your words:\n");
+		reverseInput(NULL, NULL);
 
-		char * buffer;
-		size_t length=0;
-		int size =0;
-		while(size!=-1){
-			size =getline(&buffer,&length,stdin);
-			if(size ==-1)break;
-			if(currentIndex ==vectorSize){
-				resizedVector();
-			}
-			char* tmpBuffer= (char*)malloc(sizeof(char)*size);
-			memcpy(tmpBuffer,buffer,size);
-			stringVector[currentIndex]=tmpBuffer;
-			printf("%s",stringVector[currentIndex]);
-			currentIndex++;
-
-			printf("%s",buffer); 
-		}
-		//currentIndex--;
-		printf("\n%d\n",currentIndex);
-		printf("\n---------------------");
-		
-		for(int i =0;i<currentIndex;++i){
-			char* temp= stringVector[i];
-			printf("%d %s",i,temp);
-		}
-		//printList();
-		//printf("\n---------------------");
-		
 	}
 	else
 	{
+		FILE *outputf=NULL;
+		FILE *inputf=NULL;
 		char* inputName=argv[1];
 		char* outputName;
-		if(argc >2){
+		if(argc >2){//user provided two files
 			outputName=argv[2];		
 			if(strcmp(inputName, outputName )==0){
 				fprintf(stderr, "Input and output file must differ\n");
 				exit(1);
-			}			
-			//printf("%s\n%s\n",input, output);
+			}	
+		//try to open out file			
+			outputf = fopen(outputName,"w");
+			if(outputf==NULL){
+				fprintf(stderr,"reverse: cannot open file %s\n",outputName);
+				exit(1);
+			}
 		}
-		else
+		else//user only provided one file
 		{
 			outputName = NULL;
 		}
-		//printf("%s\n%s\n",inputName, outputName);
 		
-		FILE *inputf= fopen(inputName,"r");
+		inputf= fopen(inputName,"r");//try to open input file	
 		if(inputf==NULL){
 			fprintf(stderr,"reverse: cannot open file %s\n",inputName);
 			exit(1);
 		}
-		// char ** lineBuffer;
-		// linerBuffer = (char**)malloc(sizeof(char*)*10);
-		
-		//read file
-		printScreen(inputf, NULL);
 
-
+		//
+		reverseInput(inputf, outputf);
 		
+		
+		if(outputf !=NULL){fclose(outputf);}
 		fclose(inputf);
 		
 		
 	}
 		
-	printf("end of file\n");
+	
 	return 0;
 }
